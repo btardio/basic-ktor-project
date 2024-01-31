78fe630f81e53e7324407f9ed8d527517a91033a
Brandon Tardio <btardio@gmail.com>
2024-01-30 20:21:10 -0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,5 @@
 # sudo yum install -y java-17-amazon-corretto-headless
 # sudo /application/gradlew build
 docker build /application/ -t kotlin-ktor-starter
-docker-compose -f /application/docker-compose.yaml up
+docker-compose -f /application/docker-compose.yaml down
+docker-compose -f /application/docker-compose.yaml up --detach
