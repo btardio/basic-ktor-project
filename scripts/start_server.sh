47a9584ac64a53c7edaf0147abbf4e5af0a6b034
Brandon Tardio <btardio@gmail.com>
2024-02-01 17:34:42 -0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,4 @@
 # sudo /application/gradlew build
 docker build /application/ -t kotlin-ktor-starter
 docker-compose -f /application/docker-compose.yaml down
-export $(grep -v '^#' .env | xargs -d '\n')
-docker-compose -f /application/docker-compose.yaml up --detach
+docker-compose -f /application/docker-compose.yaml --env-file /.env up --detach
