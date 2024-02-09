b08b48d3cf7b8a1c03cc42eaaa2170be84aa3e9f
Brandon Tardio <btardion@conceptplusllc.com>
2024-02-09 22:22:52 +0000

    updates start server

--- README.md
+++ README.md
@@ -7,7 +7,6 @@
 
 ## Set up
 
-
 ### Local Development
 
 ```
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 # sudo yum install -y java-17-amazon-corretto-headless
 # sudo /application/gradlew build
 #docker build  /application/ -t kotlin-ktor-starter --no-cache
-docker-compose -f /application/docker-compose.yaml --env-file /.env down
-docker-compose -f /application/docker-compose.yaml --env-file /.env build
-docker-compose -f /application/docker-compose.yaml --env-file /.env up --detach
+docker-compose -f /application/docker-compose.yml --env-file /.env down
+docker-compose -f /application/docker-compose.yml --env-file /.env build
+docker-compose -f /application/docker-compose.yml --env-file /.env up --detach
