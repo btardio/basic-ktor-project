10d6506c0460c30ed2f7dd67cde8f45fd8cf323a
Brandon Tardio <btardion@conceptplusllc.com>
2024-02-12 03:02:08 +0000

    updates zoo env

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,8 +4,18 @@
 #docker build  /application/ -t kotlin-ktor-starter --no-cache
 
 sudo rm -f /application/local.env
-cp /application/environment-aws/$(echo ${IPADDRA}) /env
-cp "/application/environment-aws/$IPADDRA" "/env"
+
+
+source /root/.bashrc
+
+rm -f /env
+cp --force /application/environment-aws/$(echo ${IPADDRA}) /env
+# cp -f "/application/environment-aws/$IPADDRA" "/env"
+
+echo $IPADDRA
+
+outfile=`cat /env`
+echo $outfile
 
 docker-compose -f /application/docker-compose.yml --env-file /env down
 docker-compose -f /application/docker-compose.yml --env-file /env build
