210cef57586eecc386a464ff019539eea3f0a6f2
Brandon Tardio <btardion@conceptplusllc.com>
2024-02-13 02:54:24 +0000

    updates

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -30,6 +30,18 @@ docker container stop solr2
 docker container stop solr3
 docker container stop solr1
 
+docker container rm httpd
+docker container rm zoo3
+docker container rm application-rabbit-1
+docker container rm kotlin-ktor-analyzer
+docker container rm zoo2
+docker container rm kotlin-ktor-collector
+docker container rm zoo1
+docker container rm kotlin-ktor-server
+docker container rm solr2
+docker container rm solr3
+docker container rm solr1
+
 docker-compose -f /application/docker-compose.yml --env-file /env down
 docker-compose -f /application/docker-compose.yml --env-file /env build
 docker-compose -f /application/docker-compose.yml --env-file /env up --detach
