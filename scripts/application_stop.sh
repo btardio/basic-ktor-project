4a9407d5425e606d33ee6df659f10a7bbc8d594f
Brandon Tardio <btardion@conceptplusllc.com>
2024-02-21 02:05:55 +0000

    scripts update aws

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 # docker-compose down
+
+docker container stop application-httpdproxy-1
+docker container stop httpd
+docker container stop application-rabbit-1
+docker container stop kotlin-ktor-analyzer
+docker container stop kotlin-ktor-collector
+docker container stop zoo1
+docker container stop kotlin-ktor-server
+docker container stop solr2
+docker container stop solr3
+docker container stop solr1
+
+docker container rm application-httpdproxy-1
+docker container rm httpd
+docker container rm application-rabbit-1
+docker container rm kotlin-ktor-analyzer
+docker container rm kotlin-ktor-collector
+docker container rm zoo1
+docker container rm kotlin-ktor-server
+docker container rm solr2
+docker container rm solr3
+docker container rm solr1
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -16,32 +16,6 @@ rm -f /env
 sh /application/environment-aws/$(echo ${IPADDRA})
 # cp -f "/application/environment-aws/$IPADDRA" "/env"
 
-echo $IPADDRA
-
-docker container stop httpd
-docker container stop zoo3
-docker container stop application-rabbit-1
-docker container stop kotlin-ktor-analyzer
-docker container stop zoo2
-docker container stop kotlin-ktor-collector
-docker container stop zoo1
-docker container stop kotlin-ktor-server
-docker container stop solr2
-docker container stop solr3
-docker container stop solr1
-
-docker container rm httpd
-docker container rm zoo3
-docker container rm application-rabbit-1
-docker container rm kotlin-ktor-analyzer
-docker container rm zoo2
-docker container rm kotlin-ktor-collector
-docker container rm zoo1
-docker container rm kotlin-ktor-server
-docker container rm solr2
-docker container rm solr3
-docker container rm solr1
-
 docker-compose -f /application/docker-compose.aws.yml --env-file /env down
 docker-compose -f /application/docker-compose.aws.yml --env-file /env build
 docker-compose -f /application/docker-compose.aws.yml --env-file /env up --detach
