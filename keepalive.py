c2e1c0d80a5036590faf95fcb10d63204ebfd13b
Brandon Tardio <btardion@conceptplusllc.com>
2024-03-04 00:23:51 +0000

    updates

--- keepalive.py
+++ keepalive.py
@@ -7,6 +7,7 @@ import time
 import curses
 
 def main(stdscr):
+    sendInterval = 9.0
 
     scheduled = {}
 
@@ -18,26 +19,22 @@ def main(stdscr):
     
     curses.start_color()
 
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+#     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
     stdscr.keypad(True)
     
     last = time.time() + 1
 
     stdscr.nodelay(True)
-    retry = False
-    A = ''
-    B = 'NOT FOUND'
-    BBB = ''
-    BBBB = ''
+
     while(True):
         stdscr.clear()
 
         printdictA = sorted(scheduled, key=lambda _: (scheduled[_]['timeStart']))
         printdictA.reverse()
-        #stdscr.addstr(13,13,'asdf') # str(printdictA))
-        for n in range(0,min(len(printdictA),30)):
+
+        for n in range(0,min(len(printdictA),stdscr.getmaxyx()[0])):
             stdscr.addstr(0 + n, 60,
                 str(printdictA[n]) +
                 ': TimeStart:' +
@@ -49,13 +46,14 @@ def main(stdscr):
  
 
         time.sleep(1)
-        stdscr.addstr(10,10,'Last: ' + str(last))
-        stdscr.addstr(11,10,'Next: ' + str(time.time()))
-        stdscr.addstr(0,3,A)
-        stdscr.addstr(1,3,B, curses.color_pair(1))
-        stdscr.addstr(1,4,BBB)
-        stdscr.addstr(1,5,BBBB)
-        if ( retry or last + 9 < time.time() ):
+        stdscr.addstr(8,10,'Last: ' + str(last))
+        stdscr.addstr(9,10,'Next: ' + str(last + sendInterval))
+        stdscr.addstr(10,10,'Current: ' + str(time.time()))
+
+        stdscr.addstr(1,3,'Request Interval In Seconds: ' + str(sendInterval), curses.color_pair(1))
+        stdscr.addstr(3,3,'Use + and - keys, q to exit.')
+
+        if ( last + sendInterval < time.time() ):
 
             try:
                 A = str(requests.get('http://netty.netoxena.com/startKmeans/000.png').json()['schedule_uuid'])
@@ -67,50 +65,25 @@ def main(stdscr):
             scheduledEntry['timeEnd'] = None
             scheduledEntry['timeToComplete'] = None
             scheduled[A] = scheduledEntry
-            retry = False
-            B = 'NOT FOUND'
-            BBB = ''
-            BBBB = ''
             last = time.time()
-        # get res uuid
-#            stdscr.addstr(0,3,res)
-#            uuid = "0"
-
-    #        time.sleep(1)
         try:
             for i in requests.get('http://netty.netoxena.com/getAllSchedules').json():
-                #stdscr.addstr(7, 55, str('finished' in i['jsonData']))  # ucla rocneck you want buy above the neck surgery? california lobe clear you want buy missile
-                # we are insured using our 6 million investors chronic
-                #stdscr.addstr(8, 56, str(i['schedule_uuid'] in scheduled))
-                #stdscr.addstr(9, 57, str(i['schedule_uuid']))
-                #stdscr.addstr(10, 58, str(scheduled.keys()))
                 if i['schedule_uuid'] in scheduled:
-
-                    #stdscr.addstr(11, 59, 'asdf')#json.loads(i['jsonData']))
-                    #scheduled[i['schedule_uuid']]['timeEnd'] = 111
-                    if 'True' == str('finished' in i['jsonData']): # 'finished' in i['jsonData']:    # == json.loads(i['jsonData'])['status']:
-                        #stdscr.addstr(13, 60, i['jsonData'])
-                        #stdscr.addstr(14, 61, scheduled[i['schedule_uuid']])
-                        #stdscr.addstr(15, 62, scheduled[i['schedule_uuid']]['timeEnd'])
+                    if 'True' == str('finished' in i['jsonData']):
                         if (scheduled[i['schedule_uuid']]['timeEnd'] == None):
                             scheduled[i['schedule_uuid']]['timeEnd'] = time.time()
-
                             scheduled[i['schedule_uuid']]['timeToComplete'] = scheduled[i['schedule_uuid']]['timeEnd'] - scheduled[i['schedule_uuid']]['timeStart']
-
-                        #B = 'FOUND'
-                        #BBB = str(A)
-                        #BBBB = str(i['schedule_uuid'])
         except:
             pass
-        #stdscr.addstr(0,0,str(res))
 
         stdscr.refresh()
 
         try:
             c = stdscr.getkey()
-            #stdscr.addstr(5,5,str(c))
-            if ( c == 'r' ):
-                retry = True
+            if ( c == '+'):
+                sendInterval += 0.5
+            if ( c == '-'):
+                sendInterval -= 0.5
             if ( c == 'q' ):
                 curses.nocbreak()
                 stdscr.keypad(False)
